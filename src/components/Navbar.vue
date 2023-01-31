89bfc567c14627a0224e78b8bd6326eb97bf9116
afonime royal <afonimeroyal@gmail.com>
2023-01-31 12:13:10 +0100

    First Commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+    
+          <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center justify-content-between">
+        <div class="d-flex">
+            <div class="d-flex align-items-center">
+        <a href="index.html" class="logo me-3"><img src="../assets/newlogo.jpg" alt="" class="img-fluid"></a>
+    </div>
+        <div class="d-flex align-items-center">
+      <h1 class="logo"><a href="index.html">EARLY IMPRESSIONS </a></h1>
+        </div>
+      <!-- Uncomment below if you prefer to use an image logo -->
+    </div>
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#services">Pricing</a></li>
+          <!-- <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li> -->
+          <li><a class="nav-link scrollto" href="#team">Team</a></li>
+          <li class="dropdown"><a href="#"><span>Categories</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Tutor Deployment</a></li>
+              <li><a href="#">Academic Instructor</a></li>
+              <li><a href="#">Special Needs</a></li>
+              <li><a href="#">Extra Curricular Instruction</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+          <li><a class="getstarted scrollto" href="#about">Sign Up</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+    
+</template>
+
+<script>
+export default {
+    name : 'Navbar'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
